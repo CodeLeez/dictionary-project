c196bb9dea9e12392eb14779ee7a4b3f47831d87
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-10-27 10:19:25 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Dictionary Project Initial Commit
+    </div>
+  );
+}
+
+export default App;
