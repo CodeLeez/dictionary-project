78ede7b101a9801befe982722d27b4bcbdd77985
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2025-10-27 11:22:56 +0800

    Dictionary Search Engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Dictionary Project Initial Commit
+      <div className="container">
+        <header className="App-header"></header>
+        Dictionary Project Initial Commit
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">Coded by Elizabeth</footer>
+        </small>
+      </div>
     </div>
   );
 }
